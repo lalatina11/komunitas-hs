7593d33885831f3be971911e9e830cc59c3e9cd5
lalatina11 <rahmadancandra@gmail.com>
2025-06-19 20:14:55 +0700

    chore: add 3nd section on land

--- index.php
+++ index.php
@@ -38,6 +38,21 @@
                 </div>
                 <img src="/assets/images/tentang-kami.svg" class="w-[83.2%] m-auto" alt="">
             </section>
+            <section id="pusatbantuan" class="relative flex flex-col justify-center items-center mt-20">
+                <img src="assets/images/pusat-bantuan/110679.svg" alt="">
+                <img src="assets/images/pusat-bantuan/Group 3.svg" class="absolute left-20 top-28" alt="">
+                <img src="assets/images/pusat-bantuan/Frame 27.svg" class="absolute left-[303px] top-[149px]" alt="">
+                <img src="assets/images/pusat-bantuan/Frame 28.svg" class="absolute left-[294px] top-[238px]" alt="">
+                <img src="assets/images/pusat-bantuan/Frame 26.svg" class="absolute left-[342px] top-[394px]" alt="">
+                <div class="absolute left-[647px] top-[105px] h-[334px] flex flex-col justify-between">
+                    <button class="btn rounded-full w-fit btn-accent px-6">⦁ Jelajahi</button>
+                    <span class="font-[800] text-[40px] text-[#FAFF9A]">Temukan Lebih Banyak Komunitas</span>
+                    <span class="font-[600] text-[20px] text-primary-content">Banyak pengguna yang ingin menjelajahi
+                        berbagai daerah hanya dari rumah, kami hadir untuk mempermudah akses semua pengguna dalam satu
+                        aplikasi</span>
+                    <a href="login.php" class="btn btn-warning w-fit">Gabung Sekarang ➚</a>
+                </div>
+            </section>
         </section>
     </main>
 </body>
