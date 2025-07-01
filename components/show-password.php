b208cdcd3884f474745647c4d42a0caf37b1ac04
lalatina11 <rahmadancandra@gmail.com>
2025-07-01 18:55:06 +0700

    chore: new compt show password button

---
+++ components/show-password.php
@@ -0,0 +1,4 @@
+<div class="flex gap-3 items-center mb-3 text-zinc-500">
+    <input class="checkbox" id="showPassword" onclick="password.type = password.type === 'text'?'password':'text'" type="checkbox">
+    <label for="showPassword">Show Password</label>
+</div>
